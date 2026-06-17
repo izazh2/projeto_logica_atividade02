f1b0a89c2c2e4197e48b9bc059804f6f034301ac
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-06-16 21:29:41 -0300

    Criado o arquivo js/script_questao02.js

---
+++ js/script_questao02.js
@@ -0,0 +1,14 @@
+const btnNum = document.querySelector('#btn-num')
+const Div01 = document.querySelector('#div-01')
+
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click', (evt) =>{
+    for(i = 1; i <= 1000; i++){
+        if( i % 7 == 0){
+            cont++
+        }
+    }
+    Div01.innerHTML =`${cont} os números divisíveis por 7`
+})
