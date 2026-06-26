d97c2be943f213bc4baf54cd987bc01c7b5c07bd
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-06-25 21:21:38 -0300

    Diversas alterações no arquivo

--- js/script_questao05.js
+++ js/script_questao05.js
@@ -9,46 +9,35 @@ let cont2000 = 0
 let contM = 0
 let acum = 0.0
 
-//capturar o evento submit no form
-formPessoa.addEventListener('submit', (evt)=>{
-    //interromper o evento padrão
-    evt.preventDefault()
 
-    //capturando o a renda digitada para acumular
+formPessoa.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
     let numDigitado = Number(inputRenda.value)
 
-    //contar o numero de pessoas registradas
     contPessoas++
-
-    //criando o objeto data form
+   
     const dadosForm = new FormData(formPessoa)
 
-    //criando e preenchendo o objeto literal pessoa
     const pessoa = {
         idade: dadosForm.get('idade'),
         sexo: dadosForm.get('sexo'),
         renda: dadosForm.get('renda')
     }
 
-    //limpar o form
     formPessoa.reset()
 
-    //se pessoa masculino acumular valor digitado
     if(pessoa.sexo === 'masculino'){
         acum += numDigitado
     }
 
-    //contar o numero de homens
     if(pessoa.sexo === 'masculino'){
         contM++
     }
 
-    //contar mulheres com renda entre 1000 e 3000
     if((pessoa.sexo === 'feminino') && (pessoa.renda >=1000) && (pessoa.renda) <= 3000){
         contFr++
     }
 
-    //contar as pessoas com superior ou igual a 2000
     if(2000 <= pessoa.renda){
         cont2000++
     }
